0e4a68110a54ad3327a7b40a049a046c0bd6f443
soyeungjo <ok69531@gmail.com>
2023-02-10 21:47:20 +0900

    add metric

--- module/argument.py
+++ module/argument.py
@@ -6,6 +6,7 @@ def get_parser():
     parser.add_argument('--tg_num', type = int, default = 403, help = 'oecd test guideline number (403, 412, 413)')
     parser.add_argument('--inhale_type', type = str, default = 'vapour', help = 'route of administration (vapour, aerosol)')
     parser.add_argument('--model', type = str, default = 'logistic', help = 'logistic, lda, qda, plsda, dt, rf, gbt, xgb, lgb, mlp')
+    parser.add_argument('--metric', type = str, default = 'f1', help = 'precision, recall, accuracy, f1, auc(only binary)')
     parser.add_argument('--num_run', type = int, default = 10, help = 'the number of run')
     
     return parser
