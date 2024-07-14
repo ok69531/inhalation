44c804a8b981adf96723694261aa9b7609a820c5
조소영 <ok69531@gmail.com>
2024-07-14 18:42:19 +0900

    recon

--- bayes_optim.py
+++ bayes_optim.py
@@ -1,4 +1,3 @@
-#%%
 import os
 import json
 import logging
@@ -7,7 +6,7 @@ import warnings
 import numpy as np
 import pandas as pd
 from tqdm import tqdm
-from imblearn.over_sampling import SMOTE
+from collections import Counter
 
 from sklearn.metrics import (
     precision_score,
@@ -33,65 +32,171 @@ from module.common import (
 from sklearn.linear_model import LogisticRegression
 from sklearn.tree import DecisionTreeClassifier
 from sklearn.ensemble import RandomForestClassifier
+from sklearn.discriminant_analysis import QuadraticDiscriminantAnalysis
 from sklearn.neural_network import MLPClassifier
+from xgboost import XGBClassifier
+
+from skopt import BayesSearchCV
+from skopt.space import Real, Categorical, Integer
+
+from imblearn.over_sampling import SMOTE
+from imblearn.pipeline import Pipeline as ImbPipeline
 
 
 warnings.filterwarnings('ignore')
 logging.basicConfig(format='', level=logging.INFO)
 
 
-#%%
-parser = get_parser()
-try:
-    args = parser.parse_args()
-except:
-    args = parser.parse_args([])
 
-args.fp_type = 'maccs'
-args.cat3tohigh = True
+def main():
+    parser = get_parser()
+    try:
+        args = parser.parse_args()
+    except:
+        args = parser.parse_args([])
 
 
-#%%
-from skopt.space import Real, Categorical, Integer
+    if (args.tg_num == 403) & (args.inhale_type == 'aerosol'):
+        args.model = 'qda'
+        args.fp_type = 'maccs'
+        args.add_md = True
+        args.cat3tohigh = True
+    elif (args.tg_num == 403) & (args.inhale_type == 'vapour'):
+        args.model = 'xgb'
+        args.fp_type = 'morgan'
+        args.add_md = True
+        args.cat3tohigh = True
+    elif (args.tg_num == 412) & (args.inhale_type == 'aerosol'):
+        # rdkit과 rdkit-md의 f1-score가 performance가 동일. auc가 더 높은 rdkit-md 사용
+        args.model = 'logistic'
+        args.fp_type = 'rdkit'
+        args.add_md = True
+        args.cat3tohigh = False
+    elif (args.tg_num == 412) & (args.inhale_type == 'vapour'):
+        args.model = 'mlp'
+        args.fp_type = 'maccs'
+        args.add_md = False
+        args.cat3tohigh = False
+    elif (args.tg_num == 413) & (args.inhale_type == 'aerosol'):
+        # lda, dt, gbt 세 모델의 performance가 동일. auc가 제일 높은 dt 사용
+        args.model = 'dt'
+        args.fp_type = 'maccs'
+        args.add_md = True
+        args.cat3tohigh = False
+    elif (args.tg_num == 413) & (args.inhale_type == 'vapour'):
+        args.model = 'rf'
+        args.fp_type = 'morgan'
+        args.add_md = True
+        args.cat3tohigh = False
 
-logging.info('=================================')
-logging.info('tg{} {} {}'.format(args.tg_num, args.inhale_type, args.model))
-logging.info('Fingerprints: {}, Use Descriptors: {}'.format(args.fp_type, args.add_md))
+    logging.info('=================================')
+    logging.info('tg{} {} {}'.format(args.tg_num, args.inhale_type, args.model))
+    logging.info('Fingerprints: {}, Use Descriptors: {}'.format(args.fp_type, args.add_md))
 
 
-if (args.tg_num == 403) & (args.inhale_type == 'aerosol'):
-    args.model = 
-    args.fp_type = 
-    args.add_md = 
-    args.cat3tohigh = True
-elif (args.tg_num == 403) & (args.inhale_type == 'vapour'):
-    args.model = 
-    args.fp_type = 
-    args.add_md = 
-    args.cat3tohigh = True
-elif (args.tg_num == 412) & (args.inhale_type == 'aerosol'):
-    # rdkit과 rdkit-md의 f1-score가 performance가 동일. auc가 더 높은 rdkit-md 사용
-    args.model = 'logistic'
-    args.fp_type = 'rdkit'
-    args.add_md = True
-    args.cat3tohigh = False
-elif (args.tg_num == 412) & (args.inhale_type == 'vapour'):
-    args.model = 'mlp'
-    args.fp_type = 'maccs'
-    args.add_md = False
-    args.cat3tohigh = False
-elif (args.tg_num == 413) & (args.inhale_type == 'aerosol'):
-    # lda, dt, gbt 세 모델의 performance가 동일. auc가 제일 높은 dt 사용
-    args.model = 'dt'
-    args.fp_type = 'maccs'
-    args.add_md = True
-    args.cat3tohigh = False
-elif (args.tg_num == 413) & (args.inhale_type == 'vapour'):
-    args.model = 'rf'
-    args.fp_type = 'morgan'
-    args.add_md = True
-    args.cat3tohigh = False
+    x, y = load_data(path = 'data', args = args)
+    if args.cat3tohigh:
+        y = new_multiclass2binary(y, args.tg_num)
+    else:
+        y = multiclass2binary(y, args.tg_num)
 
+    x_train, x_test, y_train, y_test = data_split(x, y, args.splitseed)
+
+    if args.add_md:
+        if args.fp_type == 'maccs':
+            fp_length = 167
+        elif args.fp_type == 'toxprint':
+            fp_length = 729
+        elif args.fp_type == 'morgan':
+            fp_length = 1024
+        else:
+            fp_length = 2048
+
+        train_descriptors = x_train.iloc[:, fp_length:]
+        descriptors_colnames = train_descriptors.columns
+        
+        logging.info('Number of Descriptors: {}'.format(len(descriptors_colnames)))
+        
+        scaler = MinMaxScaler()
+        scaled_train_descriptors = pd.DataFrame(scaler.fit_transform(train_descriptors, y_train))
+        scaled_train_descriptors.columns = descriptors_colnames
+        x_train.iloc[:, fp_length:] = scaled_train_descriptors
+
+        scaled_test_descriptors = pd.DataFrame(scaler.transform(x_test.iloc[:, fp_length:]))
+        scaled_test_descriptors.columns = descriptors_colnames
+        x_test.iloc[:, fp_length:] = scaled_test_descriptors
+
+
+    pipeline = ImbPipeline([
+        ('smote', SMOTE(random_state = args.smoteseed, k_neighbors = args.neighbor)),
+        ('classifier', load_model(args.model))
+    ])
+
+    best_params_list = []
+
+    for seed in tqdm(range(args.num_run)):
+        search_space = get_search_space(args.model, seed)
+        
+        opt = BayesSearchCV(
+            estimator = pipeline,
+            search_spaces = search_space,
+            cv = 5,
+            n_iter = 100
+        )
+        opt.fit(x_train, y_train)
+        
+        best_params_list.append(frozenset(opt.best_params_.items()))
+
+    test_prec_list = []
+    test_rec_list = []
+    test_f1_list = []
+    test_acc_list = []
+    test_auc_list = []
+
+    for i in range(len(best_params_list)):
+        best_params = dict(best_params_list[i])
+        best_params = {k.split('__')[-1]: v for k, v in best_params.items()}
+        
+        smote = SMOTE(random_state = args.smoteseed, k_neighbors = args.neighbor)
+        x_train, y_train = smote.fit_resample(x_train, y_train)
+        
+        model = load_model(args.model, **best_params)
+        model.fit(x_train, y_train)
+        
+        pred = model.predict(x_test)
+        pred_prob = model.predict_proba(x_test)[:, 1]
+        
+        test_prec_list.append(precision_score(y_test, pred))
+        test_rec_list.append(recall_score(y_test, pred))
+        test_f1_list.append(f1_score(y_test, pred))
+        test_acc_list.append(accuracy_score(y_test, pred))
+        test_auc_list.append(roc_auc_score(y_test, pred_prob))
+
+    checkpoints = {
+        'params': best_params_list,
+        'precision': test_prec_list,
+        'recall': test_rec_list,
+        'accuracy': test_acc_list,
+        'auc': test_acc_list
+    }
+    
+    save_path = os.path.join('bayes_optim', f'tg{args.tg_num}_{args.inhale_type}_{args.fp_type}_md{args.add_md}.json')
+    json.dump(checkpoints, open(save_path, 'w'))
+    
+    logging.info("best param: {}".format(best_params_list))
+    logging.info("precisions: {}".format(test_prec_list))
+    logging.info("recalls: {}".format(test_rec_list))
+    logging.info("accuracies: {}".format(test_acc_list))
+    logging.info("aucs: {}".format(test_auc_list))
+    logging.info("f1s: {}".format(test_f1_list))
+    
+    logging.info("test result")
+    logging.info("precision: {:.3f}({:.3f})".format(np.mean(test_prec_list), np.std(test_prec_list)))
+    logging.info("recall: {:.3f}({:.3f})".format(np.mean(test_rec_list), np.std(test_rec_list)))
+    logging.info("accuracy: {:.3f}({:.3f})".format(np.mean(test_acc_list), np.std(test_acc_list)))
+    logging.info("auc: {:.3f}({:.3f})".format(np.mean(test_auc_list), np.std(test_auc_list)))
+    logging.info("f1: {:.3f}({:.3f})".format(np.mean(test_f1_list), np.std(test_f1_list)))
+    
 
 def load_model(model: str, **kwargs):
     if model == 'logistic':
@@ -103,28 +208,14 @@ def load_model(model: str, **kwargs):
     elif model == 'rf':
         return RandomForestClassifier(**kwargs)
     
-    # elif model == 'gbt':
-    #     clf = GradientBoostingClassifier(random_state = seed, **param)
+    elif model == 'xgb':
+        return XGBClassifier(**kwargs)
     
-    # elif model == 'xgb':
-    #     clf = XGBClassifier(random_state = seed, **param)
+    elif model == 'qda':
+        return QuadraticDiscriminantAnalysis(**kwargs)
     
-    # elif model == 'lgb':
-    #     clf = LGBMClassifier(random_state = seed, **param)
-    
-    # elif model == 'lda':
-    #     clf = LinearDiscriminantAnalysis(**param)
-    
-    # elif model == 'qda':
-    #     clf = QuadraticDiscriminantAnalysis(**param)
-    
-    # elif model == 'plsda':
-    #     clf = PLSRegression(**param)
-        
     elif model == 'mlp':
         return MLPClassifier(**kwargs)
-    
-    # return clf
 
 
 def get_search_space(model_name, seed):
@@ -137,18 +228,30 @@ def get_search_space(model_name, seed):
         }
     elif model_name == 'dt':
         params = {
-            'criterion': ['gini', 'entropy'],
-            'max_depth': Integer(1, 5),
-            'min_samples_split': Integer(2, 5),
-            'min_samples_leaf': Integer(1, 5)
+            'classifier__criterion': ['gini', 'entropy'],
+            'classifier__max_depth': Integer(1, 5),
+            'classifier__min_samples_split': Integer(2, 5),
+            'classifier__min_samples_leaf': Integer(1, 5)
         }
     elif model_name == 'rf':
         params = {
-            'n_estimators': Integer(5, 150),
-            'criterion': ['gini'],
-            'min_samples_split': Integer(1, 4),
-            'min_samples_leaf': Integer(1, 3),
-            'max_features': ['sqrt', 'log2']
+            'classifier__n_estimators': Integer(5, 150),
+            'classifier__criterion': ['gini'],
+            'classifier__min_samples_split': Integer(1, 4),
+            'classifier__min_samples_leaf': Integer(1, 3),
+            'classifier__max_features': ['sqrt', 'log2']
+        }
+    elif model_name == 'xgb':
+        params = {
+            'classifier__min_child_weight': Integer(1, 5),
+            'classifier__max_depth': Integer(3, 9), 
+            'classifier__gamma': (0, 3),
+            'classifier__booster': ['gbtree']
+        }
+    elif model_name == 'qda':
+        params = {
+            'classifier__reg_param': (0, 1),
+            'classifier__tol': (1e-5, 1e-3)
         }
     elif model_name == 'mlp':
         params = {
@@ -163,90 +266,5 @@ def get_search_space(model_name, seed):
     return params
 
 
-x, y = load_data(path = 'data', args = args)
-if args.cat3tohigh:
-    y = new_multiclass2binary(y, args.tg_num)
-else:
-    y = multiclass2binary(y, args.tg_num)
-
-x_train, x_test, y_train, y_test = data_split(x, y, args.splitseed)
-
-if args.add_md:
-    if args.fp_type == 'maccs':
-        fp_length = 167
-    elif args.fp_type == 'toxprint':
-        fp_length = 729
-    elif args.fp_type == 'morgan':
-        fp_length = 1024
-    else:
-        fp_length = 2048
-
-    train_descriptors = x_train.iloc[:, fp_length:]
-    descriptors_colnames = train_descriptors.columns
-    
-    logging.info('Number of Descriptors: {}'.format(len(descriptors_colnames)))
-    
-    scaler = MinMaxScaler()
-    scaled_train_descriptors = pd.DataFrame(scaler.fit_transform(train_descriptors, y_train))
-    scaled_train_descriptors.columns = descriptors_colnames
-    x_train.iloc[:, fp_length:] = scaled_train_descriptors
-
-    scaled_test_descriptors = pd.DataFrame(scaler.transform(x_test.iloc[:, fp_length:]))
-    scaled_test_descriptors.columns = descriptors_colnames
-    x_test.iloc[:, fp_length:] = scaled_test_descriptors
-
-
-#%%
-from imblearn.over_sampling import SMOTE
-from imblearn.pipeline import Pipeline as ImbPipeline
-
-from collections import Counter
-from skopt import BayesSearchCV
-
-
-pipeline = ImbPipeline([
-    ('smote', SMOTE(random_state = args.smoteseed, k_neighbors = args.neighbor)),
-    ('classifier', load_model(args.model))
-])
-
-best_params_list = []
-
-for seed in tqdm(range(1)):
-# for seed in tqdm(range(args.num_run)):
-    search_space = get_search_space(args.model, seed)
-    opt = BayesSearchCV(
-        estimator = pipeline,
-        search_spaces = search_space,
-        cv = 5,
-        n_iter = 10
-    )
-    opt.fit(x_train, y_train)
-    
-    best_params_list.append(frozenset(opt.best_params_.items()))
-
-# %%
-test_prec_list = []
-test_rec_list = []
-test_f1_list = []
-test_acc_list = []
-test_auc_list = []
-
-# smote = SMOTE(random_state = args.smoteseed, k_neighbors = args.neighbor)
-# train_x, train_y = smote.fit_resample(train_x, train_y)
-for i in range(len(best_params_list)):
-    best_params = dict(best_params_list[i])
-    best_params = {k.split('__')[-1]: v for k, v in best_params.items()}
-    
-    model = load_model(args.model, **best_params)
-    
-
-
-model = LogisticRegression(**dict(best_params_list[0]))
-model.fit(x_train, y_train)
-pred = model.predict(x_test)
-
-precision_score(y_test, pred)
-recall_score(y_test, pred)
-f1_score(y_test, pred)
-accuracy_score(y_test, pred)
-# roc_auc_score(y_train, pred_score)
+if __name__ == '__main__':
+    main()
